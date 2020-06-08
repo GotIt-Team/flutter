10a8c8ceae616691e1c27e00a0050650884e4959
Asmaakhaled <52568588+Azmaa22@users.noreply.github.com>
2020-06-09 00:15:20 +0200

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart';
-
+import 'AppTheme.dart';
 import 'views/pages/user_profile_view.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+MyTheme appTheme ;
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      
-      ),
+      theme: appTheme.IsDarker(false),
       home:HomePage()
       
     );
