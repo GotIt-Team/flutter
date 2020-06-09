d1892a3b1e320a86736415a6da474a571a7a2dc3
Abdalrahman Ahmed <abdalrahmana574@gmail.com>
2020-06-09 11:11:18 +0200

    add app theme handler (need fix colors)

---
+++ lib/AppTheme.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ThemeHandler with ChangeNotifier {
+  bool _isDark = false;
+
+  bool get isDark {
+    return _isDark;
+  }
+
+  void switchTheme(bool isDark) {
+    _isDark = isDark;
+    notifyListeners();
+  }
+}
+
+class AppTheme {
+  static ThemeHandler themeHandler = ThemeHandler();
+
+  static final ThemeData lightTheme = ThemeData(
+    backgroundColor: Color(0xD1D0B5),
+    scaffoldBackgroundColor: Color(0xD1D0B5),
+    appBarTheme: AppBarTheme(
+      color: Color(0x458C9E),
+      iconTheme: IconThemeData(
+        color: Colors.white,
+      ),
+    ),
+    iconTheme: IconThemeData(
+      color: Colors.white54,
+    ),
+    buttonTheme: ButtonThemeData(
+      buttonColor: Colors.red, //text on button
+      hoverColor: Colors.blue,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15)
+      ),
+      textTheme: ButtonTextTheme.accent
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(5)),
+        borderSide: BorderSide(color: Colors.blue)
+      )
+    ),
+    textTheme: TextTheme(
+      title: TextStyle(
+        color: Colors.white,
+        fontSize: 20.0,
+      ),
+      subtitle: TextStyle(
+        color: Colors.white70,
+        fontSize: 18.0,
+      ),
+    ),
+  );
+
+  static final ThemeData darkTheme = ThemeData(
+    primaryColor: Colors.brown,
+    backgroundColor: Colors.brown,
+    scaffoldBackgroundColor: Colors.brown,
+    appBarTheme: AppBarTheme(
+      color: Colors.black,
+      iconTheme: IconThemeData(
+        color: Colors.black,
+      ),
+    ),
+    iconTheme: IconThemeData(
+      color: Colors.black,
+    ),
+    textTheme: TextTheme(
+      title: TextStyle(
+        color: Colors.black,
+        fontSize: 20.0,
+      ),
+    ),
+  );
+
+  static ThemeMode get currentMode {
+    return themeHandler.isDark ? ThemeMode.dark : ThemeMode.light;
+  }
+
+  static void switchTheme(bool isDark) {
+    themeHandler.switchTheme(isDark);
+  }
+}
+
+
+// app colors
+/***
+ * system bar color & primary color (#3B7585)
+ * app bar color (#458C9E)
+ * app background (#D1D0B5)
+ * text primary color (#000000)
+ * sub title color (#BDC3C7)
+***/
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,36 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+import 'AppTheme.dart';
 
-class MyApp extends StatelessWidget {
+void main() => runApp(App());
+
+class App extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return AppState();
+  }
+  
+}
+
+
+class AppState extends State<App> {
+  @override
+  void initState(){
+    super.initState();
+    AppTheme.themeHandler.addListener((){
+      setState(() {});
+    });
+  }
+  
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      //home:
-      ),
-      
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: AppTheme.currentMode,
+      //home: 
     );
   }
 }
