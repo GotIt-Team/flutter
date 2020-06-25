0ea200167e662d4e5a61c23688ae4d03f7034cb2
Abdalrahman Ahmed <abdalrahmana574@gmail.com>
2020-06-25 13:12:08 +0200

    revert work on user settings page

--- lib/views/pages/user_requests_tab.dart
+++ lib/views/pages/user_requests_tab.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gotit/views/widgets/empty_state.dart';
 
 class UserRequestsTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('User requests')
+    return EmptyState(
+      image: 'assets/images/no_requests.png',
+      message: 'There is no requests',
     );
   }
   
