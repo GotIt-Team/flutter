b856f809959b6d586f39d0898f60965d4e8c512a
Abdalrahman Ahmed <abdalrahmana574@gmail.com>
2020-06-28 21:46:15 +0200

    create bubble widget

---
+++ lib/views/widgets/bubble.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class Bubble extends StatelessWidget {
+  final AlignmentGeometry alignment;
+  final Widget child;
+  final Color backgroundColor;
+  Bubble({this.alignment, this.child, this.backgroundColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10),
+      alignment: alignment,
+      child: Container(
+        constraints: BoxConstraints(
+          maxWidth: MediaQuery.of(context).size.width * 0.80
+        ),
+        padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.01),
+        margin: EdgeInsets.symmetric(vertical: MediaQuery.of(context).size.height * 0.005),
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 5,
+            ),
+          ]
+        ),
+        child: child
+      ),
+    );
+  }
+}
